5991f642e7e2fe9539f8d632a3b1046cc6fb2f2d
GA <guillaume.abramovici@outlook.fr>
2025-07-31 11:31:20 +0200

    multiple html entry vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-multi-html'
 
 export default defineConfig({
-  root: '.',
   base: './',
+  plugins: [
+    createHtmlPlugin({
+      pages: {
+        index: 'index.html',
+        about: 'about.html',
+        contact: 'contact.html',
+        projects: 'projects.html',
+      }
+    })
+  ]
 })
